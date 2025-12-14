cd2dc4604231dbd27432b1f7242132a2448015f1
Hlamong55 <bijoymarma55@gmail.com>
2025-12-15 00:01:25 +0600

    ui modifies in package card section

--- src/pages/Home/Packages/PackagesSection.jsx
+++ src/pages/Home/Packages/PackagesSection.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaCheckCircle } from "react-icons/fa";
 
 
 const PackagesSection = () => {
@@ -49,31 +50,33 @@ const PackagesSection = () => {
             <div
               key={plan._id}
               className="
-                relative rounded-2xl border border-base-300 bg-white p-8
+                relative rounded-2xl border border-base-300 bg-blue-100 p-8
                 shadow-sm transition-all duration-200
                 hover:-translate-y-1 hover:shadow-lg
               "
             >
-              <h3 className="text-2xl font-bold text-center text-gray-900">
+              <h3 className="text-3xl font-bold text-center text-gray-900">
                 {plan.name}
               </h3>
+
+              <div className="border mt-1"></div>
 
               {/* Price */}
               <p className="mt-4 text-center text-4xl font-extrabold text-[#2563eb]">
                 ${plan.price}
-                <span className="text-lg text-gray-500 font-medium"> /month</span>
+                <span className="text-lg text-gray-700 font-medium"> /month</span>
               </p>
 
-              <p className="mt-1 text-center text-gray-600">
-                Up to <span className="font-semibold">{plan.employeeLimit}</span> employees
+              <p className="mt-1 text-center text-gray-700">
+                Up to <span className="font-bold">{plan.employeeLimit}</span> employees
               </p>
 
-              <div className="my-6 border-t border-base-200"></div>
+              <div className="my-5 border"></div>
 
               <ul className="space-y-3 text-gray-700">
                 {plan.features.map((feature, idx) => (
                   <li key={idx} className="flex items-center gap-3">
-                    <span className="w-2 h-2 rounded-full bg-[#4FAE33]"></span>
+                    <FaCheckCircle className="text-primary bg-white rounded-full text-lg" />
                     {feature}
                   </li>
                 ))}
